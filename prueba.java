f4ae16bbc806c7445d94c608d551e4de2b4d2499
unknown <scampo1997@gmail.com>
2022-09-26 10:18:09 +0200

    Ahora también se muestra la edad y el año de nacimiento

--- prueba.java
+++ prueba.java
@@ -3,6 +3,8 @@ public class prueba {
 	public static void main(String args[]){
 
 		System.out.println("Apellidos: Campo Rodríguez");
+		System.out.println("Edad: 25");
+		System.out.println("Año de nacimiento: 1997");
 
 		
 	}
