9dd786389f53c9f7fdb1cbad2e06359472856296
unknown <scampo1997@gmail.com>
2022-09-27 09:14:59 +0200

    Quitados apellidos y añadido el grupo favorito

--- prueba.java
+++ prueba.java
@@ -2,10 +2,11 @@ public class prueba {
 
 	public static void main(String args[]){
 
-		System.out.println("Apellidos: Campo Rodríguez");
+		
 		System.out.println("Edad: 25");
 		System.out.println("Año de nacimiento: 1997");
 		System.out.println("Comida favorita: costilla");
+		System.out.println("Grupo favorito: Breaking Benjamin");
 
 		
 	}
